a536e8adf9a677d2cb0e08f3e1a868c8c228a939
Joscha <Joscha19@gmail.com>
2021-05-15 11:43:59 +0200

    initial

---
+++ CameraApp/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Windows;
+
+namespace CameraApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            var connectionString = Configuration.GetConnectionString("MyDatabase");
+
+            Console.WriteLine(connectionString);
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            services.AddTransient<MainWindow>(_ => new MainWindow(Configuration));
+        }
+    }
+}
---
+++ CameraApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Windows;
+
+namespace CameraApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IConfiguration Configuration { get; }
+
+        public MainWindow(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            InitializeComponent();
+
+            ReadAllSettings();
+        }
+
+        private void ReadAllSettings()
+        {
+            try
+            {
+                var connectionString = Configuration.GetConnectionString("MyDatabase");
+
+                MyTextBlock.Text += "connectionString: " + connectionString + "\n";
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error reading app settings");
+                MyTextBlock.Text += "Error reading app settings.\n";
+            }
+        }
+    }
+}
